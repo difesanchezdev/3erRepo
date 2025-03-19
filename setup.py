26032350cb6c15552664b80ef2ca3e1d2da4a585
difesanchez <difesanchedev@gmail.com>
2025-03-19 11:06:00 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="3erRepo",                                 # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Diego F Sanchez G",                     # Tu nombre
+    author_email="difesanchezdev@gmail.com",        # Tu correo electrónico
+    url="https://github.com/difesanchezdev/3erRepo",# URL del proyecto
+)
